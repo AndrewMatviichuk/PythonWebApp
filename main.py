5e5b8c9e8c14279a2e30e8837721826e80f00e98
AndrewMatviichuk <58109902+AndrewMatviichuk@users.noreply.github.com>
2020-03-31 01:42:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from starlette.requests import Request
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def method(request: Request):
+	return {"method": request.method}
