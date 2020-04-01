8fe295b2a8abc66ae45818b48de8f7340586aecf
AndrewMatviichuk <58109902+AndrewMatviichuk@users.noreply.github.com>
2020-04-01 15:28:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,5 +8,9 @@ def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
 @app.get("/method")
+@app.post("/method")
+@app.put("/method")
+@app.delete("/method")
 def method(request: Request):
 	return {"method": request.method}
+
