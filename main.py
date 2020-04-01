789a3f18d845daee52cbb2509c38b181abced2d1
AndrewMatviichuk <58109902+AndrewMatviichuk@users.noreply.github.com>
2020-04-01 16:38:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 from starlette.requests import Request
 
+class PatientRq(BaseModel):
+	name: str
+	surname: str
+
+class PatientResp(BaseModel):
+	id: int
+	patient: dict
+
+
 app = FastAPI()
+patient_counter = 0
 
 @app.get("/")
 def root():
@@ -13,4 +24,10 @@ def root():
 @app.delete("/method")
 def method(request: Request):
 	return {"method": request.method}
+
+
+@app.post("/patient", response_model=PatientResp)
+def add_patient(patientRq: PatientRq):
+	patient_counter += 1
+	return PatientResp(id=patient_counter, patient=patientRq.dict())
 
