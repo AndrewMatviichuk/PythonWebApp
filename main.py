833103d2d75c186dec79254bda640051c9d75811
AndrewMatviichuk <58109902+AndrewMatviichuk@users.noreply.github.com>
2020-04-28 02:08:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -131,3 +131,16 @@ async def get_tracks(page: int = 0, per_page: int = 10):
     data = app.db_connection.execute(
         "SELECT * FROM tracks ORDER BY TrackId LIMIT ? OFFSET ?", (per_page, page*per_page,)).fetchall()
     return data
+
+
+@app.get("/tracks/composers")
+async def get_tracks(composer_name: str = ""):
+    app.db_connection.row_factory = sqlite3.Row
+    data = app.db_connection.execute(
+        "SELECT Name FROM tracks WHERE Composer = ? ORDER BY Name", (composer_name,)).fetchall()
+    if not data:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={"error": "Not found!"},
+        )
+    return data
